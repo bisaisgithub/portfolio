7e24c979278751fe2ab3362110b7a2c76e061914
bisaisgithub <benaremail@gmail.com>
2022-09-13 12:31:20 +0800

    fix responsive sample component

--- components/notification/ButtonToast.js
+++ components/notification/ButtonToast.js
@@ -1,7 +1,6 @@
 import styles from '../../styles/ButtonToast.module.scss';
 
 const ButtonToast = ({children, handleClick, color}) => {
-  console.log('color', color)
   return (<button 
     className={styles.button}
     style={{backgroundColor: color  }}
