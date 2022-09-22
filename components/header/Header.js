e32f6e4d7edb54e3dd64c4187c4f6427e3b0bde5
bisaisgithub <benaremail@gmail.com>
2022-09-23 02:29:53 +0800

    change pic and some reponsiveness

--- components/header/Header.js
+++ components/header/Header.js
@@ -3,19 +3,21 @@ import { useState } from "react";
 import styles from "../../styles/Header.module.scss";
 
 const Header = () => {
-  const [rightValue, setRightValue] = useState('-200');
+  const [rightValue, setRightValue] = useState("-200");
   return (
-    <header className={styles.header} id='home'>
+    <header className={styles.header} id="home">
       <div className={styles.container}>
         <nav className={styles.nav}>
-          <h3>Ben<span>Ar</span></h3>
+          <h3>
+            Ben<span>Ar</span>
+          </h3>
           {/* <img className={styles.logo} src="./images/logo.png" alt="logo" /> */}
-          <ul 
-            style={{right: 
-              `${rightValue}px`
+          <ul
+            style={{
+              right: `${rightValue}px`,
               // '-00px'
             }}
-            >
+          >
             <li>
               <a href="#home">Home</a>
             </li>
@@ -26,18 +28,32 @@ const Header = () => {
               <a href="#projects">Projects</a>
             </li>
             <li>
-              <Link href="/sample-components" passHref><a>Components</a></Link>
+              <Link href="/sample-components" passHref>
+                <a>Components</a>
+              </Link>
             </li>
             <li>
               <a href="#contact">Contact</a>
             </li>
-            <i className="fa-solid fa-xmark" onClick={()=>setRightValue('-200')}></i>
+            <i
+              className="fa-solid fa-xmark"
+              onClick={() => setRightValue("-200")}
+            ></i>
           </ul>
-          <i className="fa-solid fa-bars" onClick={()=>setRightValue('0')}></i>
+          <i
+            className="fa-solid fa-bars"
+            onClick={() => setRightValue("0")}
+          ></i>
         </nav>
         <div className={styles.headerText}>
-          <p>Web Developer</p>
-          <h1>Hi, I{`'`}m <br/> <span>Benar</span> Isais</h1>
+          {/* <p>Aspiring Web Developer</p> */}
+          <div>
+            <h1>
+              Hi, I{`'`}m <br /> <span>Benar</span> Isais
+            </h1>
+            <p>Aspiring Web Developer</p>
+          </div>
+          <img src="./images/benHeader3.png" alt="" />
         </div>
       </div>
     </header>
